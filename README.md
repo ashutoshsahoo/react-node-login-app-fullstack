fb826cc44c6883dd30c5c811bc98e26c5f34f095
ashutoshsahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2021-05-09 22:57:37 +0530

    add files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-node-login-app-fullstack
+
 Login application with react as frontend and Nodejs as backend
---
+++ node-js-jwt-auth-mongodb/app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "emp_database",
+  USER: 'emp_user',
+  PWD: 'emp_pwd123',
+};
